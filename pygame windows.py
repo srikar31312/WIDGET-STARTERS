dcaffe9b610814e6bb1e121b5b519632fef0696b
srikar31312 <vadlurisrikar@gmail.com>
2025-03-30 20:16:08 +0530

    PYGAME WINDOW

---
+++ pygame windows.py
@@ -0,0 +1,11 @@
+# Import necessary libraries
+from tkinter import *
+
+# Create window
+window = Tk()
+
+# set the window title and geometry
+window.title('Demo Window')
+window.geometry('400x300')
+
+window.mainloop()
